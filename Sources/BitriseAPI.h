c1ac38ee44c6e464f596c2c16531781cacd397d9
Joel Trew <contact@joeltrew.com>
2018-02-10 21:56:14 +0000

    Initial Commit

---
+++ Sources/BitriseAPI.h
@@ -0,0 +1,18 @@
+//
+//  BitriseAPI_iOS.h
+//  BitriseAPI iOS
+//
+//  Created by Joel Trew on 10/02/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BitriseAPI.
+FOUNDATION_EXPORT double BitriseAPI_VersionNumber;
+
+//! Project version string for BitriseAPI.
+FOUNDATION_EXPORT const unsigned char BitriseAPI_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitriseAPI/PublicHeader.h>
+
+
---
+++ Sources/ExampleClass.swift
@@ -0,0 +1,12 @@
+//
+//  ExampleClass.swift
+//  BitriseAPI iOS
+//
+//  Created by Joel Trew on 10/02/2018.
+//
+
+import Foundation
+
+public class ExampleClass {
+    
+}
